65485ae1ee786a93070f5f893fe2f387b78b6e3b
Saloni Jibhe <jibhesaloni@gmail.com>
2026-09-16 19:55:38 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://interview-ai-tk0c.onrender.com",
+    origin: "https://interview-ai-frontend-mu-lovat.vercel.app/login",
     credentials: true
 }))
 
